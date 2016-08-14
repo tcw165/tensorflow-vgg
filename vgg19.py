2094e962ad834463766cd274baaf67c2343a007f
Boy Wang <boyw165@gmail.com>
2016-08-14 14:43:53 +0800

    Add more comments.

--- vgg19.py
+++ vgg19.py
@@ -1,9 +1,3 @@
-"""
-A VGG-19 Network implementation using TensorFlow library.
-
-Author: machrisaa, boyw165
-"""
-
 import numpy as np
 import tensorflow as tf
 
@@ -11,6 +5,24 @@ _VGG_MEAN = [103.939, 116.779, 123.68]
 
 
 class Vgg19:
+    """
+    A VGG-19 Network implementation using TensorFlow library.
+    The network takes an image of size 224x224 with RGB channels and returns
+    category scores of size 1000.
+
+    The network configuration:
+    - RGB: 224x224x3
+    - BGR: 224x224x3
+    - conv1: 224x224x64
+    - conv2: 112x112x128
+    - conv3: 56x56x256
+    - conv4: 28x28x512
+    - conv5: 14x14x512
+    - fc6: 25088(=7x7x512)x4096
+    - fc7: 4096x4096
+    - fc8: 4096x1000
+    """
+
     WIDTH = 224
     "The fixed width of the input image."
     HEIGHT = 224
@@ -83,6 +95,9 @@ class Vgg19:
                  model_save_freq=0):
         """
         :param model: The model either for back-propagation or
+        :param model_save_path: The model path for training process.
+        :param model_save_freq: Save the model (in training process) every N
+        iterations.
         forward-propagation.
         """
         self.model = self._init_empty_model() if not model else model
@@ -94,6 +109,7 @@ class Vgg19:
                                             [None, 1000])
 
         # Define the input placeholder with RGB channels.
+        # Size: 224x224x3
         self._inputRGB = tf.placeholder(tf.float32,
                                         [None,
                                          Vgg19.WIDTH,
@@ -101,6 +117,7 @@ class Vgg19:
                                          Vgg19.CHANNELS])
 
         # Convert RGB to BGR order
+        # Size: 224x224x3
         red, green, blue = tf.split(3, 3, self._inputRGB)
         self._inputBGR = tf.concat(3, [
             blue,
@@ -110,6 +127,7 @@ class Vgg19:
 
         # normalize the input so that the elements all have nearly equal
         # variances.
+        # Size: 224x224x3
         self._inputNormalizedBGR = tf.concat(3, [
             blue - _VGG_MEAN[0],
             green - _VGG_MEAN[1],
@@ -117,38 +135,51 @@ class Vgg19:
         ])
 
         # Setup the VGG-Net graph.
+        # Size: 224x224x64
         self._conv1_1 = self._conv_layer(self._inputNormalizedBGR, "conv1_1")
         self._conv1_2 = self._conv_layer(self._conv1_1, "conv1_2")
+        # Size: 112x112x64
         self._pool1 = self._max_pool(self._conv1_2, 'pool1')
 
+        # Size: 112x112x128
         self._conv2_1 = self._conv_layer(self._pool1, "conv2_1")
         self._conv2_2 = self._conv_layer(self._conv2_1, "conv2_2")
+        # Size: 56x56x128
         self._pool2 = self._max_pool(self._conv2_2, 'pool2')
 
+        # Size: 56x56x256
         self._conv3_1 = self._conv_layer(self._pool2, "conv3_1")
         self._conv3_2 = self._conv_layer(self._conv3_1, "conv3_2")
         self._conv3_3 = self._conv_layer(self._conv3_2, "conv3_3")
         self._conv3_4 = self._conv_layer(self._conv3_3, "conv3_4")
+        # Size: 28x28x256
         self._pool3 = self._max_pool(self._conv3_4, 'pool3')
 
+        # Size: 28x28x512
         self._conv4_1 = self._conv_layer(self._pool3, "conv4_1")
         self._conv4_2 = self._conv_layer(self._conv4_1, "conv4_2")
         self._conv4_3 = self._conv_layer(self._conv4_2, "conv4_3")
         self._conv4_4 = self._conv_layer(self._conv4_3, "conv4_4")
+        # Size: 14x14x512
         self._pool4 = self._max_pool(self._conv4_4, 'pool4')
 
+        # Size: 14x14x512
         self._conv5_1 = self._conv_layer(self._pool4, "conv5_1")
         self._conv5_2 = self._conv_layer(self._conv5_1, "conv5_2")
         self._conv5_3 = self._conv_layer(self._conv5_2, "conv5_3")
         self._conv5_4 = self._conv_layer(self._conv5_3, "conv5_4")
+        # Size: 7x7x512
         self._pool5 = self._max_pool(self._conv5_4, 'pool5')
 
+        # Size: 25088(=7x7x512)x4096
         self._fc6 = self._fc_layer(self._pool5, "fc6")
         self._relu6 = tf.nn.relu(self._fc6)
 
+        # Size: 4096x4096
         self._fc7 = self._fc_layer(self._relu6, "fc7")
         self._relu7 = tf.nn.relu(self._fc7)
 
+        # Size: 4096x1000
         self._fc8 = self._fc_layer(self._relu7, "fc8")
 
         # For predicting.
@@ -253,6 +284,7 @@ class Vgg19:
 
     def _init_empty_model(self):
         self.model = {
+            # All the following things follows [0] = weights, [1] = biases.
             # Conv-layer 1.
             "conv1_1": [np.ndarray([3, 3, 3, 64]),
                         np.ndarray([64])],
