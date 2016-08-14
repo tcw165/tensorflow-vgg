13b51d8303c580d8a45be35b6229c39bf9534878
Boy Wang <boyw165@gmail.com>
2016-08-14 14:44:52 +0800

    Add `vgg16.npy`.

--- examples/test_lib.py
+++ examples/test_lib.py
@@ -6,4 +6,5 @@ sys.path.append(os.path.abspath("../"))
 
 tf_log_path = os.path.abspath("./log/test")
 model_vgg19 = os.path.abspath("../models/vgg19.npy")
+model_vgg16 = os.path.abspath("../models/vgg16.npy")
 label_vgg = os.path.abspath("../models/synset.txt")
